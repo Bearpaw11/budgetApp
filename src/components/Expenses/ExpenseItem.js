fe40103155cdb13b603ba24095b5dc9c1456342b
Chris Behrens <chrisbehrens@yahoo.com>
2022-01-11 12:27:46 -0700

    feat: added Button to card on ExpenseItem.js with an onCLick listener

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,9 @@ import './ExpenseItem.css';
 
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log('Clicked!!!');
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
@@ -11,6 +14,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
