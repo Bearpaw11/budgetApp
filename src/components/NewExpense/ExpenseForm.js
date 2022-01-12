b1ca4106b375638d10a31a5f30ff26100d44f1ff
Chris Behrens <chrisbehrens@yahoo.com>
2022-01-12 12:29:25 -0700

    feat: added onSubmit handler to gather form data. ExpenseForm.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -18,8 +18,18 @@ const ExpenseForm = () => {
         setEnteredDate(event.target.value)
     };
     
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+       console.log(expenseData) 
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className= "new-expense__control">
                     <label>Title</label>
@@ -27,7 +37,7 @@ const ExpenseForm = () => {
                 </div>
                 <div className= "new-expense__control">
                     <label>Amount</label>
-                    <input type='number' min="0.01" step =".0.01" onChange={amountChangeHandler} />
+                    <input type='number' min="0.01" step ="0.01" onChange={amountChangeHandler} />
                 </div>
                 <div className= "new-expense__control">
                     <label>Date</label>
