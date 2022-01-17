a398c597b19f454fb04d2b7a0584283ac56ddaf9
Chris Behrens <chrisbehrens@yahoo.com>
2022-01-16 20:36:22 -0700

    feat: passed selected Year object to Expenses.js via onChangeFilter

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,20 +1,20 @@
-import React from 'react';
 import './ExpensesFilter.css';
 
-const ExpensesFilter = () => {
-  const dropdownChangeHandler = (event) => {
-    console.log(event.target.value)
-}
+const ExpensesFilter = (props) => {
+ 
+ const dropdownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value)
+  };
 
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select onChange={dropdownChangeHandler}>
+        <select value= {props.selected} onChange={dropdownChangeHandler}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
-          <option value='2019'>Emersyn </option>
+          <option value='2019'>2019</option>
         </select>
       </div>
     </div>
