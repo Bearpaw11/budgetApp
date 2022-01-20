7b7a88c6dbd5c11387a21e79b2aa4bfde936063d
Chris Behrens <chrisbehrens@yahoo.com>
2022-01-19 20:52:27 -0700

    feat: added filteredExpenses function to expenses.js to render on selected year expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,13 +10,16 @@ const Expenses = (props) => {
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   }  
-  
+ 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <div>
     <Card className="expenses">
       <ExpensesFilter selected = {filteredYear} onChangeFilter ={filterChangeHandler} />
-      {props.items.map(expense => (
+      {filteredExpenses.map(expense => (
       <ExpenseItem 
       key = {expense.id}
       title = {expense.title} 
